9e9489b8ef7e91c5d869228af82229fc3c8c5e55
james.joe <kyeun950830@gmail.com>
2023-07-19 14:38:09 +0900

    Feat:startPage (mp4 background & visit button)

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,42 @@
-import React from 'react';
-import About from './About';
-import Archiving from './Archiving';
-import Skills from './Skills';
+import React, { useState } from "react";
+import About from "./About";
+import Banner from "./Banner";
+import Archiving from "./Archiving";
+import Skills from "./Skills";
+import "./Pages.css";
+import waves from "../videos/waves.mp4";
 
 export default function Home() {
+  const [start, setStart] = useState(false);
+  const handleStart = () => {
+    setStart((prev) => !prev);
+  };
+  if (!start) {
     return (
-        <div>
-            <About/>
-            <Skills/>
-            <Archiving/>
-            
-        </div>
-    );
-}
+      <div>
+        <div className="start__wrapper">
+            <div className="overlay"></div>
+          <video className="start__video" src={waves} autoPlay loop muted />
+          <div className="start__container">
 
+            <h1>Welcome</h1>
+            <div>James site</div>
+          <button className="start__button" onClick={handleStart}>
+                <div>Visit</div>
+          </button>
+          
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Banner />
+        <About />
+        <Skills />
+        <Archiving />
+      </div>
+    );
+  }
+}
